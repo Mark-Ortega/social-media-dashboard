4643907d441267a8e3f7bd474cd749ab2269f582
Mark Ortega <ortegamark847@gmail.com>
2024-03-24 12:55:35 -0700

    Fixed margin issues and added padding to my cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import HeaderCards from './Components/CardHeader';
+import Overview from './Components/Overview';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='bg-lightMode-veryPaleBlueTop dark:bg-darkMode-veryDarkBlueBgTop w-full h-72 absolute z-0 rounded-lg'></div>
+    <div className='bg-lightMode-whiteBg dark:bg-darkMode-veryDarkBlueBg h-full z-10'>
+      <Header/>
+      <HeaderCards/>
+      <Overview/>
+    </div>
     </div>
   );
 }
